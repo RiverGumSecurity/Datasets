4f90563048a2f80a084dcd40d13a7ef89b9acb2f
Joff Thyer <joff@blackhillsinfosec.com>
2024-11-07 13:37:07 -0500

    fixed up clean data logic for blog parsing into tokens

--- scripts/cleanup_data.py
+++ scripts/cleanup_data.py
@@ -10,8 +10,9 @@ from bs4 import BeautifulSoup
 class Convert():
 
     output_json = []
-    def __init__(self, filename, ins=''):
+    def __init__(self, filename, ins='', maxlen=1024):
         self.filename = filename
+        self.maxlen = maxlen - 40
         self.ins = ins
 
     def run(self):
@@ -25,10 +26,7 @@ class Convert():
             taxonomies = html.unescape(
                 re.sub(r'[^\x20-\x5b\x5d-\x7a]+', '', ', '.join(b['taxonomies'])).strip())
             created = b['date_created']
-            content = re.sub(r' +', ' ', BeautifulSoup(b['content'], "html.parser").get_text())
-            content = re.sub(r'\n{2,}', '\n\n', content)
-            content = re.sub(r'^\n', '', content)
-            content = re.sub(r'\u00a0', ' ', content)
+            output = re.sub(r' +', ' ', BeautifulSoup(b['content'], "html.parser").get_text())
             if created is None:
                 created = ''
             inp = f"""\
@@ -37,20 +35,35 @@ Taxonomies: "{taxonomies}"
 Creation Date: "{created}"
 """
 
-            if len(content) < 200 or \
-                    re.match(r'^(?:\[embed\])?https?://.+', content) or \
+            output = re.sub(r' +', ' ', BeautifulSoup(b['content'], "html.parser").get_text())
+            if len(output) < 200 or \
+                    re.match(r'^(?:\[embed\])?https?://.+', output) or \
                     'webcast' in taxonomies.lower() or \
                     'podcast' in taxonomies.lower():
                 continue
 
-            self.output_json.append({
+            parsed = self.parse_content(inp, output)
+            for item in parsed:
+                self.output_json.append({
                     'input': '', 
                     'instruction': self.ins,
-                    'output': inp + content
+                    'output': item
                 })
-
         print(json.dumps(self.output_json, indent=4))
 
+    def parse_content(self, prefix, content):
+        res = []
+        content = re.sub(r'(?i)https?://[a-z0-9\-\.]+?', '', content)
+        tokens = re.findall(r"([\-\w'!?\.]+)\b", content)
+
+        if len(tokens) > self.maxlen:
+            for i in range(len(tokens) // self.maxlen):
+                s = i * self.maxlen
+                e = i * self.maxlen + self.maxlen
+                res.append(prefix + ' '.join(tokens[s:e]))
+        else:
+            res.append(prefix + ' '.join(tokens))
+        return res
 
 
 if __name__ == '__main__':
@@ -65,5 +78,8 @@ in the information security domain.  Using the input provided, pick from one of 
 '''
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', help='json file of input data')
+    parser.add_argument(
+            '-m', '--maxlen', type=int,
+            default=4096, help='max output len')
     args = parser.parse_args()
-    Convert(args.filename, ins=instructions).run()
+    Convert(args.filename, ins=instructions, maxlen=args.maxlen).run()
